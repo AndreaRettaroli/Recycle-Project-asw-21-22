6c4a7cd15b216926a878556de807eb0c371fb41e
andrea rettaroli <andrea.rettaroli@studio.unibo.it>
2023-03-01 21:28:10 +0100

    feat: :construction: add user controller

---
+++ backend/controllers/userController.js
@@ -0,0 +1,6 @@
+exports.signup = (req, res) => {};
+exports.login = (req, res) => {};
+exports.getUser = (req, res) => {};
+exports.updateUser = (req, res) => {};
+exports.deleteUser = (req, res) => {};
+exports.usersList = (req, res) => {};
