55786e289b2a43c21bba5dac8560ba1156b02225
andrea rettaroli <andrea.rettaroli@studio.unibo.it>
2023-03-03 20:49:56 +0100

    feat: :construction: add user model

---
+++ backend/models/userModel.js
@@ -0,0 +1,37 @@
+module.exports = (mongoose) => {
+  const userSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    surname: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    province: {
+      type: String,
+      required: true,
+    },
+    language: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+  });
+  return mongoose.model("Users", userSchema);
+};
