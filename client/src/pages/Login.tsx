eec0117206fd837526caf374ff55121035ff966c
andrea rettaroli <andrea.rettaroli@studio.unibo.it>
2023-03-10 20:57:32 +0100

    feat: :construction: add basic form

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,7 +1,80 @@
-import React from "react";
+import React, { FC } from "react";
+import { useForm, Resolver } from "react-hook-form";
+import api from "../api/Request";
 
-const Login = () => {
-  return <h1 className="text-3xl font-bold underline">Login!</h1>;
+interface LoginFormData {
+  email: string;
+  password: string;
+}
+
+const Login: FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormData>({
+    email: "",
+    password: "",
+  });
+
+  const onSubmit = (data: LoginFormData) => {
+    console.log(data);
+    login(data);
+  };
+
+  const login = async (data: LoginFormData) => {
+    const response = await api.post("/api/login", data);
+    console.log("🚀 ~ file: Login.tsx:26 ~ login ~ response:", response);
+  };
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <form className="w-full max-w-sm" onSubmit={handleSubmit(onSubmit)}>
+        <h1 className="text-3xl font-bold mb-6">Login</h1>
+        <div className="mb-4">
+          <label htmlFor="email" className="block text-gray-700 font-bold mb-2">
+            Email
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="email"
+            type="email"
+            placeholder="Enter your email"
+            {...register("email", { required: true })}
+          />{" "}
+          {errors.email && (
+            <p className="text-red-500 text-sm">{errors.email.message}</p>
+          )}
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="password"
+            className="block text-gray-700 font-bold mb-2"
+          >
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="Enter your password"
+            {...register("password", { required: true })}
+          />{" "}
+          {errors.password && (
+            <p className="text-red-500 text-sm">{errors.password.message}</p>
+          )}
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Sign In
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
