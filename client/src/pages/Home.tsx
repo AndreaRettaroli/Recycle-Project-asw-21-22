d15f0d6c8353710888f4ad3658abed2dd3ec6cb3
andrea rettaroli <andrea.rettaroli@studio.unibo.it>
2023-03-12 21:09:28 +0100

    feat: :construction: created baskets list

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { FC, useEffect } from "react";
+import { RootState } from "../redux/store";
+import Loading from "../components/UI/Loading";
+import { useSelector, useDispatch } from "react-redux";
+import { getBaskets } from "../redux/baskets.slice";
 
-const Home = () => {
-  return <h1 className="text-3xl font-bold underline">Hone!</h1>;
+const Home: FC = () => {
+  const dispatch = useDispatch();
+  const fetchedData = useSelector(
+    (state: RootState) => state.baskets.fetchedData
+  );
+  const user = useSelector((state: RootState) => state.user.user);
+  console.log("🚀 ~ file: Home.tsx:13 ~ user:", user)
+  // useEffect(() => {
+  //   if (!fetchedData) {
+  //     dispatch(getBaskets(user?._id, user?.token));
+  //   }
+  // });
+  const baskets = useSelector((state: RootState) => state.baskets.baskets);
+  console.log("🚀 ~ file: Home.tsx:19 ~ baskets:", baskets)
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Your Baskets!</h1>
+      {!fetchedData ? (
+        <Loading />
+      ) : (
+        <ul>
+          {baskets?.map((basket, index) => (
+            <li key={"basket-" + index}>{basket._id}</li>
+          ))}
+          <li>Add</li>
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default Home;
