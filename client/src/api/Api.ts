c026f8f9bb697af9d1d70bb1c8d316da7c2fc6e5
andrea rettaroli <andrea.rettaroli@studio.unibo.it>
2023-03-13 18:39:57 +0100

    feat: :zap: get token from session

--- client/src/api/Api.ts
+++ client/src/api/Api.ts
@@ -1,54 +1,24 @@
 import axios from "axios";
+import useUserSession from "../hooks/useUserSession";
 
 const DEV_HOST_BACKEND = "http://localhost:3000";
 const PROD_HOST_BACKEND = "http://localhost:3000";
+//const { isLoggedIn, loggedUser, login, logout } = useUserSession();
 
-const api = axios.create({
+const Api = axios.create({
     baseURL: DEV_HOST_BACKEND
 });
 
-api.interceptors.request.use(async (config) => {
-    const token = config?.headers?.Authorization?.toString().split(' ')[1];
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
+
+Api.interceptors.request.use(async (config) => {
+    const userSession = sessionStorage.getItem("userSession");
+    if (userSession) {
+        const { token } = JSON.parse(userSession);
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
     }
     return config;
 });
 
-export default api;
-
-
-//import axios, { AxiosInstance } from 'axios';
-// const API: AxiosInstance = axios.create({
-//   baseURL: 'http://localhost:3000/'
-// });
-
-
-// // Set the token in the Authorization header
-// const setAuthToken = (token: string) => {
-//     if (token) {
-//         api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-//     } else {
-//         delete api.defaults.headers.common["Authorization"];
-//     }
-// };
-
-// const request = <T>(
-//     method: Method,
-//     url: string,
-//     params?: any,
-//     data?: any,
-//     token?: string
-// ): Promise<AxiosResponse<T>> => {
-//     if (token) {
-//         setAuthToken(token);
-//     }
-//     return api.request<T>({
-//         method,
-//         url,
-//         params,
-//         data
-//     });
-// };
-
-
+export default Api;
