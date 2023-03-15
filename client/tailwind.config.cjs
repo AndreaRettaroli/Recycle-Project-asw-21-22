c6a7426fde727c754da5d691073404d1dac89ee2
andrea rettaroli <andrea.rettaroli@studio.unibo.it>
2023-03-15 18:26:15 +0100

    feat: :lipstick: update theme and style

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -2,7 +2,42 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+      xxl: "2048px",
+    },
+    colors: {
+      white: "#ffffff",
+      green: "#13ce66",
+      "gray-dark": "#273444",
+      gray: "#8492a6",
+      "gray-light": "#d3dce6",
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        "2xl": "1rem",
+        "4xl": "2rem",
+        "8xl": "4rem",
+      },
+      boxShadow: {
+        default:
+          "rgba(0, 0, 0, 0.1) 0px 20px 25px -5px, rgba(0, 0, 0, 0.04) 0px 10px 10px -5px",
+      },
+      minWidth: {
+        300: "300px",
+      },
+    },
   },
   plugins: [],
 };
