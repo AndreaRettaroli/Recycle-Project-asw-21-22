33289e0a2a322b005909345028806d5bb40bd567
andrea rettaroli <andrea.rettaroli@studio.unibo.it>
2023-03-16 18:51:00 +0100

    refactor: :lipstick: improve navabar and loading

--- client/src/components/UI/Navbar.tsx
+++ client/src/components/UI/Navbar.tsx
@@ -1,18 +1,57 @@
 import React, { FC } from "react";
 import { NavLink } from "react-router-dom";
+import logo from "/recycleLogo.jpg";
 
-const Navbar: FC = () => {
+interface Props {
+  title: string;
+}
+
+const Navbar: FC<Props> = ({ title }) => {
   return (
-    <nav>
-      <ul>
+    <nav className="w-full flex items-center justify-between p-2 top-0 absolute bg-white">
+      <div className="sm:flex sm:items-center hidden">
+        <img src={logo} width="80px" alt="recycle-logo" />
+        <h1 className="text-3xl font-bold">{title}</h1>
+      </div>
+      <ul className="inline-flex gap-8 items-center mr-16">
         <li>
-          <NavLink to="/home">Home</NavLink>
+          <NavLink
+            to="/home"
+            style={({ isActive }) => ({
+              fontWeight: isActive ? "Bolder" : "none",
+              textDecoration: isActive ? "underline" : "none",
+              color: isActive ? "#13ce66" : "#000000",
+            })}
+            exact
+          >
+            Home
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/statistics">Statistics</NavLink>
+          <NavLink
+            to="/statistics"
+            exact
+            style={({ isActive }) => ({
+              fontWeight: isActive ? "Bolder" : "none",
+              textDecoration: isActive ? "underline" : "none",
+              color: isActive ? "#13ce66" : "#000000",
+            })}
+          >
+            Statistics
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/profile">Profile</NavLink>
+          <NavLink
+            to="/profile"
+            style={({ isActive }) => ({
+              fontWeight: isActive ? "Bolder" : "none",
+              textDecoration: isActive ? "underline" : "none",
+              color: isActive ? "#13ce66" : "#000000",
+            })}
+            exact
+          >
+            Profile
+          </NavLink>
         </li>
       </ul>
     </nav>
