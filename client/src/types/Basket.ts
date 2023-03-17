b059401ae2f27987daafbe41f3a22ed8844416fa
andrea rettaroli <andrea.rettaroli@studio.unibo.it>
2023-03-17 17:00:08 +0100

    feat: :wrench: change basket types settings

--- client/src/types/Basket.ts
+++ client/src/types/Basket.ts
@@ -1,11 +1,24 @@
 export interface Basket {
     _id: string
     userId: string
-    type: string//"PLASTIC" | "ORGANIC" | "PAPER" | "GLASS" | "METAL" | "UNDIFFERENTIED"
-    dimension: number
+    type: BasketTypes//"PLASTIC" | "ORGANIC" | "PAPER" | "GLASS" | "METALS" | "MIXED"
+    dimension: BasketDimensions
     filling: number
     createdAt: Date | null
     updatedAt: Date | undefined | null
 }
 
-export const basketTypes = [{ name: "PLASTIC" }, { name: "ORGANIC" }, { name: "PAPER" }, { name: "GLASS" }, { name: "UNDIFFERENTIED" }, { name: "METAL" }]
+export enum BasketTypes {
+    PLASTIC = "PLASTIC",
+    ORGANIC = "ORGANIC",
+    PAPER = "PAPER",
+    GLASS = "GLASS",
+    METALS = "METALS",
+    MIXED = "MIXED",
+}
+
+export enum BasketDimensions {
+    SMALL = 2,
+    MEDIUM = 5,
+    LARGE = 10,
+}
