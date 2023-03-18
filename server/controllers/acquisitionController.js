0fc6ea18abdd09025ec1f42ac0e7788af75b842c
andrea rettaroli <andrea.rettaroli@studio.unibo.it>
2023-03-18 16:13:12 +0100

    feat: :zap: add filter by basketId on acquisitions

--- server/controllers/acquisitionController.js
+++ server/controllers/acquisitionController.js
@@ -3,11 +3,15 @@ const acquisitionModel = require("../models/acquisitionModel");
 exports.acquisitionList = async (req, res) => {
   try {
     const userId = req.query.userId;
-    if (Object.is(userId, undefined) || Object.is(userId, null)) {
-      let acquisitions = await acquisitionModel.find({});
+    const basketId = req.query.basketId;
+    if (!Object.is(userId, undefined) && !Object.is(userId, null)) {
+      let acquisitions = await acquisitionModel.find({ userId: userId });
+      return res.status(200).json(acquisitions);
+    } else if (!Object.is(basketId, undefined) && !Object.is(basketId, null)) {
+      let acquisitions = await acquisitionModel.find({ basketId: basketId });
       return res.status(200).json(acquisitions);
     } else {
-      let acquisitions = await acquisitionModel.find({ userId: userId });
+      let acquisitions = await acquisitionModel.find({});
       return res.status(200).json(acquisitions);
     }
   } catch (err) {
