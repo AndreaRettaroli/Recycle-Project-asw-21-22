590179aaafa94d48f0d784fdbd52f22c0d88343e
andrea rettaroli <andrea.rettaroli@studio.unibo.it>
2023-03-20 00:27:15 +0100

    refactor: :lipstick: refactor not found page

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -1,13 +1,27 @@
 import React, { FC } from "react";
 import { Link } from "react-router-dom";
+import Button from "../components/UI/Button";
+import Card from "../components/UI/Card";
+import logo from "/recycleLogo.jpg";
 
 const NotFound: FC = () => {
   return (
-    <div>
-      <h1>Oops! You seem to be lost.</h1>
-      <p>Here are some helpful links:</p>
-      <Link to="/login">Login</Link>
-      <Link to="/home">Home</Link>
+    <div className="flex-container">
+      <Card>
+        <img src={logo} width="80px" alt="recycle-logo" />
+        <div className="w-3/6">
+          <h1 className="text-3xl font-bold">Oops! You seem to be lost.</h1>
+          <p>Here are some helpful links:</p>
+          <div className="flex flex-row gap-4">
+            <Button>
+              <Link to="/login">Login</Link>
+            </Button>
+            <Button>
+              <Link to="/home">Home</Link>
+            </Button>
+          </div>
+        </div>
+      </Card>
     </div>
   );
 };
