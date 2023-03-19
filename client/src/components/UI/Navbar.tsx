1c55b97fee65c973d58c929127414b5a8b991b5e
andrea rettaroli <andrea.rettaroli@studio.unibo.it>
2023-03-19 17:39:44 +0100

    feat: :lipstick: change navbar UI for mobile

--- client/src/components/UI/Navbar.tsx
+++ client/src/components/UI/Navbar.tsx
@@ -1,6 +1,9 @@
 import react, { FC } from "react";
 import { NavLink } from "react-router-dom";
 import logo from "/recycleLogo.jpg";
+import home from "../../assets/home.svg";
+import statistics from "../../assets/statistics.svg";
+import profile from "../../assets/profile.svg";
 
 interface Props {
   title: string;
@@ -8,50 +11,76 @@ interface Props {
 
 const Navbar: FC<Props> = ({ title }) => {
   return (
-    <nav className="shadow-header w-full flex flex-col sm:flex-row items-center justify-between p-1 fixed bottom-0 sm:top-0 sm:bottom-auto sm:top-auto bg-white">
-      <div className="sm:flex sm:items-center hidden">
+    <>
+      <div className="w-full flex justify-center items-center sm:hidden">
         <img src={logo} width="80px" alt="recycle-logo" />
         <h1 className="text-3xl font-bold">{title}</h1>
       </div>
-      <ul className="inline-flex gap-8 items-center sm:mr-16">
-        <li>
-          <NavLink
-            to="/home"
-            style={({ isActive }) => ({
-              fontWeight: isActive ? "Bolder" : "none",
-              textDecoration: isActive ? "underline" : "none",
-              color: isActive ? "#13ce66" : "#000000",
-            })}
-          >
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/statistics"
-            style={({ isActive }) => ({
-              fontWeight: isActive ? "Bolder" : "none",
-              textDecoration: isActive ? "underline" : "none",
-              color: isActive ? "#13ce66" : "#000000",
-            })}
-          >
-            Statistics
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/profile"
-            style={({ isActive }) => ({
-              fontWeight: isActive ? "Bolder" : "none",
-              textDecoration: isActive ? "underline" : "none",
-              color: isActive ? "#13ce66" : "#000000",
-            })}
-          >
-            Profile
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+      <nav className="shadow-header z-10 w-full flex flex-col sm:flex-row items-center justify-between p-1 fixed bottom-0 sm:top-0 sm:bottom-auto  bg-white">
+        <div className="sm:flex sm:items-center hidden">
+          <img src={logo} width="80px" alt="recycle-logo" />
+          <h1 className="text-3xl font-bold">{title}</h1>
+        </div>
+
+        <ul className="inline-flex gap-8 items-center sm:mr-16">
+          <li>
+            <NavLink
+              to="/home"
+              style={({ isActive }) => ({
+                fontWeight: isActive ? "Bolder" : "none",
+                textDecoration: isActive ? "underline" : "none",
+                color: isActive ? "#13ce66" : "#000000",
+              })}
+            >
+              <img
+                src={home}
+                width={24}
+                style={{ backgroundImage: home }}
+                className="sm:hidden m-auto "
+                alt="home-icon"
+              />
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/statistics"
+              style={({ isActive }) => ({
+                fontWeight: isActive ? "Bolder" : "none",
+                textDecoration: isActive ? "underline" : "none",
+                color: isActive ? "#13ce66" : "#000000",
+              })}
+            >
+              <img
+                src={statistics}
+                width={24}
+                className="sm:hidden m-auto"
+                alt="statistics-icon"
+              />
+              Statistics
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/profile"
+              style={({ isActive }) => ({
+                fontWeight: isActive ? "Bolder" : "none",
+                textDecoration: isActive ? "underline" : "none",
+                color: isActive ? "#13ce66" : "#000000",
+              })}
+            >
+              <img
+                src={profile}
+                width={24}
+                className="sm:hidden m-auto"
+                alt="statistics-icon"
+              />
+              Profile
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 };
 
