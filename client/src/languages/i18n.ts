c566fc59728ec169401281d238edd2644f1b0629
andrea rettaroli <andrea.rettaroli@studio.unibo.it>
2023-03-26 19:37:38 +0200

    feat: :zap: add methods to get language and change language

--- client/src/languages/i18n.ts
+++ client/src/languages/i18n.ts
@@ -2,6 +2,7 @@ import i18next from "i18next";
 import englishTranslation from "./en/translations.json";
 import italianTranslation from "./it/translations.json";
 import { initReactI18next } from "react-i18next";
+import { Languages } from "../types/Languages";
 
 
 
@@ -15,8 +16,17 @@ export const resources = {
   }
 } as const;
 
+const getLanguage = () => {
+  return window.navigator.language.slice(0, 2)
+}
+
+
+export const changeLanguage = (lng: Languages) => {
+  i18next.changeLanguage(lng);
+}
+
 i18next.use(initReactI18next).init({
-  lng: "it",
+  lng: getLanguage(),
   resources,
 });
 
